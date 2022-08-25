0cfad15b3fef79cfa3a09612b993d37ca7ec7ef7
YYxDeveloper <yyxdev@gmail.com>
2022-08-25 17:34:21 +0800

    init

---
+++ app/src/main/java/com/yyxdev/yyxkotlinprovingground/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,54 @@
+package com.yyxdev.yyxkotlinprovingground.ui.dashboard
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.yyxdev.yyxkotlinprovingground.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        runKotlin()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    fun runKotlin() {
+        val  a = 1
+        val  b= 2
+        if (a != b) {
+            Log.d(null,"aaaa")
+        }
+    }
+}
