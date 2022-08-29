991f31c405255cf18f95b3d32ec48034feb86661
YYxDeveloper <yyxdev@gmail.com>
2022-08-29 09:02:42 +0800

    //

--- app/src/test/java/com/yyxdev/yyxkotlinprovingground/ExampleUnitTest.kt
+++ app/src/test/java/com/yyxdev/yyxkotlinprovingground/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.yyxdev.yyxkotlinprovingground
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -49,11 +51,16 @@ class ExampleUnitTest {
             else -> println("not red") // 'else' is required
         }
     }
-    @Test
-    fun try_Array(){
-        // https://louis383.medium.com/kotlin-when-e383d642ac79
-        val number = (1..7).random()
 
+    @Test
+    fun try_list(){
+        // https://louis383.medium.com/kotlin-when-e383d642ac79
+
+        val numbers = (1..7)
+
+        val number = numbers.random()
+
+        // use When
         when (number) {
             1 -> println("Number is 1")
             2 -> println("Number is 2")
@@ -65,6 +72,20 @@ class ExampleUnitTest {
             else -> println("Other")
         }
     }
+    @Test
+    fun try_filter_list () {
+        val numList = listOf<Int>(0,1,2)
+        val numbers = (1..7)
+
+        val number = numbers.random()
+
+        //
+        val it = 5
+        val answer = numbers.filter { it > 0 }
+        println("ppppp:::{$answer}")
+    }
+
+    // enum
     enum class CurrencyType(val chineseName: String,
                             val simpleExRate: Double) {
         USD("美元", 30.0),
@@ -91,4 +112,35 @@ class ExampleUnitTest {
             println(info)
         }
     }
+    @Test
+    fun try_dataClass(){
+        data class Person(val name: String) {
+            var age: Int = 0
+            var pockets = listOf<String>("a","b")
+        }
+
+
+
+        val person1 = Person("John")
+        val person2 = Person("John")
+
+        person1.age = 10
+        person2.age = 20
+
+    }
+    @Test
+    fun try_noneNull_listFiles() {
+        val files = ("ss","dd").listFiles()
+
+        println(files?.size ?: "empty") // if files is null, this prints "empty"
+
+// To calculate the fallback value in a code block, use `run`
+        val filesSize = files?.size ?: run {
+            return someSize
+        }
+        println(filesSize)
+
+    }
 }
+
+
